3c0e9a348efda057f7bd3bc6c6791651ba491e68
Aaron Martinez <aaronmarti17@gmail.com>
2019-08-20 02:24:40 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -27,7 +27,10 @@ def find_min_value(array)
   counter = 0 
   min = array[0]
   while array[counter] do
-    
+    if array[counter] < min 
+      min = array[counter]
+    end 
     counter += 1 
   end
+  return min 
 end
